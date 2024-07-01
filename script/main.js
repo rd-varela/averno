de449bda9706ddf8e005d894fbb8b7bdc86454ec
David Varela <rd.varela@outlook.com>
2024-07-01 12:48:33 -0300

    sfx

--- script/main.js
+++ script/main.js
@@ -9,17 +9,38 @@ let firstJumpscare = true;
 let jumpscareVideo = document.getElementById('jumpscareVideo')
 let jumpscareVideoDiv = document.getElementById('jumpscareVideoDiv')
 let jumpscareSound = new Audio("https://github.com/rd-varela/averno/blob/main/sound/jumpscare.mp3?raw=true")
+let pentagramImg = document.getElementById('pentagramImg')
+let thumpSound = new Audio("https://github.com/rd-varela/averno/blob/main/sound/thump.wav?raw=true")
+
+let keystrokeSound = new Audio("https://github.com/rd-varela/averno/blob/main/sound/typewriter.wav?raw=true");
+
+name.addEventListener('input', playKeystrokeSound);
+question.addEventListener('input', playKeystrokeSound);
+
+function playKeystrokeSound() {
+    keystrokeSound.currentTime = 0;
+    keystrokeSound.play();
+}
 
 document.getElementById('pentagram').addEventListener('click', ask)
 
 
 async function ask() {
+
+    pentagramImg.src="./img/pentagram-inv.png";
+
+    thumpSound.play();
+
+    setTimeout(() => {
+        pentagramImg.src = "./img/pentagram.png";
+    }, 300);
+
     let pText = document.getElementById('response');
     pText.textContent = "";
     const prompt = "Contestame la siguiente pregunta como si fueras un espiritu. En primera persona, en estilo paranormal y serio, sin chistes, de manera breve y misteriosa sin elementos de narracion, con un acento argentino de Buenos Aires. Sos el espiritu de " + name.value + " y la pregunta es la siguiente: " + question.value
     const result = await model.generateContent(prompt);
     const response = await result.response;
-    const text = response.text();
+    const text = response.text();  
 
     pText.classList.remove('fade-in');
     void pText.offsetWidth;
@@ -50,11 +71,7 @@ async function ask() {
         await new Promise(resolve => setTimeout(resolve, 200)); // Adjust timing if needed
     }
 
-    //pText.classList.remove('fade-in');
-    //pText.textContent = text;
     pText.textContent = scrambledText;
-    //void pText.offsetWidth;
-    //pText.classList.add('fade-in');
 
     // Wait for animation to complete (1s delay as defined in CSS)
     await new Promise(resolve => setTimeout(resolve, 100));
@@ -71,20 +88,6 @@ async function ask() {
         jumpscare();
     }
 }
-
-/*function scrambleText(text) {
-    let scrambledText = '';
-    const chars = text.split('');
-
-    // Shuffle the characters randomly
-    while (chars.length) {
-        const randomIndex = Math.floor(Math.random() * chars.length);
-        scrambledText += chars[randomIndex];
-        chars.splice(randomIndex, 1);
-    }
-
-    return scrambledText;
-}*/
 
 function jumpscare(){
     jumpscareVideoDiv.style.display = 'flex';
